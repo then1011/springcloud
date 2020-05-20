7daaf06fa63076b880c81a1c253fd8e29fda5511
Then <Then378906696@qq.com>
2020-05-20 11:29:31 +0800

    增加post解析方式

--- framework-springboot/src/main/java/com/then/springboot/mvc/request/resolve/impl/PostResolveHandler.java
+++ framework-springboot/src/main/java/com/then/springboot/mvc/request/resolve/impl/PostResolveHandler.java
@@ -6,38 +6,47 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.then.core.utils.JsonUtils;
 import com.then.springboot.mvc.request.resolve.IResolveHandler;
 
-public class PostResolveHandler extends IResolveHandler{
+public class PostResolveHandler extends IResolveHandler {
 
 	@Override
 	public Object resolve(HttpServletRequest request, MethodParameter parameter) throws Exception {
-		
-		String contentType = request.getContentType();
-		
-		if("application/json".equalsIgnoreCase(contentType)) {
+
+		String contentType = request.getContentType() == null ? "" : request.getContentType();
+
+		if (contentType.equals("application/json")) {
 			StringBuilder sb = new StringBuilder();
 			String readLine = "";
-			
-			try(BufferedReader br = request.getReader()) {
-				while((readLine = br.readLine()) != null) {
+
+			try (BufferedReader br = request.getReader()) {
+				while ((readLine = br.readLine()) != null) {
 					sb.append(readLine);
 				}
 			}
-			
+
 			String paramString = sb.toString();
-			if(StringUtils.isEmpty(paramString)) {
+			if (StringUtils.isEmpty(paramString)) {
 				return null;
 			}
 			return JsonUtils.parseObject(paramString, parameter.getParameterType());
-			
-		}else if("multipart/form-data".equalsIgnoreCase(contentType)) {
-			
+
+		} else if (contentType.startsWith("multipart/form-data") && request instanceof MultipartHttpServletRequest) {
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			String paramName = parameter.getParameterName();
+			Class<?> paramClass = parameter.getParameterType();
+			if (paramClass == MultipartFile.class) {
+				return multipartRequest.getFile(paramName);
+			} else {
+				return super.getReflectionParam(request, parameter);
+			}
 		}
-		
-		return null;
+
+		return super.getReflectionParam(request, parameter);
 	}
 
 }
