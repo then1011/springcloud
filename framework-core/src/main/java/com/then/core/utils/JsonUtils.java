2230e8d48716b6c35cb89eb3a1117f866fe66dae
Then <Then378906696@qq.com>
2020-05-21 10:21:12 +0800

    提交工具类

--- framework-core/src/main/java/com/then/core/utils/JsonUtils.java
+++ framework-core/src/main/java/com/then/core/utils/JsonUtils.java
@@ -1,5 +1,7 @@
 package com.then.core.utils;
 
+import java.io.InputStream;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,7 +10,7 @@ import com.then.core.exception.SystemException;
 
 public class JsonUtils {
 	private static final ObjectMapper MAPPER = new ObjectMapper();
-	
+
 	public static String toJsonString(Object obj) {
 		try {
 			return MAPPER.writeValueAsString(obj);
@@ -16,18 +18,34 @@ public class JsonUtils {
 			throw new SystemException(SysCodeEnum.GET_SEQ_FAILURE, e);
 		}
 	}
-	
-	public static <T>T parseObject(String str, Class<T> formatClass){
+
+	public static <T> T parseObject(String str, Class<T> formatClass) {
 		try {
 			return MAPPER.readValue(str, formatClass);
 		} catch (Exception e) {
 			throw new SystemException(SysCodeEnum.GET_SEQ_FAILURE, e);
 		}
 	}
-	
-	public static <T>T parseObject(String str, TypeReference<T> valueTypeRef){
+
+	public static <T> T parseObject(String str, TypeReference<T> valueTypeRef) {
 		try {
 			return MAPPER.readValue(str, valueTypeRef);
+		} catch (Exception e) {
+			throw new SystemException(SysCodeEnum.GET_SEQ_FAILURE, e);
+		}
+	}
+
+	public static <T> T parseObject(InputStream inputStream, Class<T> formatClass) {
+		try {
+			return MAPPER.readValue(inputStream, formatClass);
+		} catch (Exception e) {
+			throw new SystemException(SysCodeEnum.GET_SEQ_FAILURE, e);
+		}
+	}
+
+	public static <T> T parseObject(InputStream inputStream, TypeReference<T> valueTypeRef) {
+		try {
+			return MAPPER.readValue(inputStream, valueTypeRef);
 		} catch (Exception e) {
 			throw new SystemException(SysCodeEnum.GET_SEQ_FAILURE, e);
 		}
